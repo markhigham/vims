0395220400fe19a4098c916a7802c5a40235745c
Mark Higham <mark.higham@gmail.com>
2025-07-05 13:22:44 +0100

    Add karabiner and aerospace

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -f ${HOME}/.aerospace.toml ]; then
+    echo "Linking .aerospace.toml"
+    ln -s ${PWD}/.aerospace.toml ${HOME}/.aerospace.toml
+fi
+
 if [ ! -f ${HOME}/.ideavimrc ]; then
     echo "Linking .ideavimrc"
     ln -s ${PWD}/.ideavimrc ${HOME}/.ideavimrc
@@ -15,4 +20,10 @@ if [ ! -d ${HOME}/.config/nvim ]; then
     echo "Linking nvim config"
     mkdir -p ${HOME}/.config
     ln -s ${PWD}/nvim ${HOME}/.config/nvim
+fi
+
+if [ ! -d ${HOME}/.config/karabiner ]; then
+    echo "Linking karabiner config"
+    mkdir -p ${HOME}/.config
+    ln -s ${PWD}/karabiner ${HOME}/.config/karabiner
 fi
